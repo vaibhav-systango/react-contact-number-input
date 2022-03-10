92bad8fb45e5166b09d86d9864c72d0b28a714f5
vaibhav-systango <vaibhav@systango.com>
2022-03-10 16:29:54 +0530

    Added storybook

---
+++ src/examples/example.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Story } from "@storybook/react";
+import {PhoneInput} from '../Components';
+export default {
+    title: "Phone Number Input",
+    component: PhoneInput,
+  } 
+  const Template = (args) => <PhoneInput {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
